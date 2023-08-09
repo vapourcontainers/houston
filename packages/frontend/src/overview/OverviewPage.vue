e4b881c26c10a6232830d75dd51c9c768a9f49d5
XiNGRZ <hi@xingrz.me>
2023-08-09 18:01:11 +0800

    Overview

--- packages/frontend/src/overview/OverviewPage.vue
+++ packages/frontend/src/overview/OverviewPage.vue
@@ -7,10 +7,10 @@
             <a-statistic title="存储" :value="storage?.value" :precision="2" :suffix="storage?.unit" />
           </a-col>
           <a-col :span="8">
-            <a-statistic title="任务" :value="2" />
+            <a-statistic title="任务" :value="taskStore?.tasks?.length" />
           </a-col>
           <a-col :span="8">
-            <a-statistic title="余额" :value="account?.balance?.availableAmount" :precision="2" prefix="￥" />
+            <a-statistic title="余额" :value="accountStore?.balance?.availableAmount" :precision="2" prefix="￥" />
           </a-col>
         </a-row>
       </a-card>
@@ -22,15 +22,19 @@
 import { onMounted } from 'vue';
 
 import { useBucketStore } from '@/stores/bucket';
+import { useTaskStore } from '@/stores/task';
 import { useAccountStore } from '@/stores/account';
 
 import useSize from '@/composables/useSize';
 
-const bucket = useBucketStore();
-onMounted(() => bucket.fetchStat());
+const bucketStore = useBucketStore();
+onMounted(() => bucketStore.fetchStat());
 
-const account = useAccountStore();
-onMounted(() => account.fetchBalance());
+const taskStore = useTaskStore();
+onMounted(() => taskStore.fetchTasks());
 
-const storage = useSize(() => bucket.stat?.storage);
+const accountStore = useAccountStore();
+onMounted(() => accountStore.fetchBalance());
+
+const storage = useSize(() => bucketStore.stat?.storage);
 </script>

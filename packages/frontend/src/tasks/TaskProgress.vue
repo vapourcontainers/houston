2b3be45b39cd310428c05a6b440af3e1d1a9bf03
XiNGRZ <hi@xingrz.me>
2023-08-14 11:02:54 +0800

    frontend: Better progress display

--- packages/frontend/src/tasks/TaskProgress.vue
+++ packages/frontend/src/tasks/TaskProgress.vue
@@ -1,23 +1,23 @@
 <template>
   <a-skeleton active :loading="!progress" :title="false">
-    <a-row :gutter="[16, 16]">
+    <a-row :gutter="[16, 16]" :class="$style.progress">
       <a-col :xs="24" :sm="12" :md="8" :xl="4">
-        <a-statistic title="帧" :value="progress?.frame" />
+        <a-statistic title="帧" :value="progress?.frame" group-separator="" :suffix="info ? `/ ${info.frames}` : ''" />
       </a-col>
       <a-col :xs="24" :sm="12" :md="8" :xl="4">
-        <a-statistic title="FPS" :value="progress?.fps" :precision="2" suffix="fps" />
+        <a-statistic title="帧率" :value="progress?.fps" :precision="2" suffix="fps" />
       </a-col>
       <a-col :xs="24" :sm="12" :md="8" :xl="4">
         <a-statistic title="大小" :value="size?.value" :precision="2" :suffix="size?.unit" />
       </a-col>
       <a-col :xs="24" :sm="12" :md="8" :xl="4">
-        <a-statistic title="时间" :value="progress?.outTime" />
+        <a-statistic title="时间码" :value="progress?.outTime" />
       </a-col>
       <a-col :xs="24" :sm="12" :md="8" :xl="4">
         <a-statistic title="码率" :value="progress?.bitrate" />
       </a-col>
       <a-col :xs="24" :sm="12" :md="8" :xl="4">
-        <a-statistic title="速度" :value="progress?.speed" :precision="3" suffix="×" />
+        <a-statistic title="速度" :value="progress?.speed" :precision="4" suffix="×" />
       </a-col>
     </a-row>
 
@@ -58,3 +58,15 @@ async function updateProgress() {
 
 useInterval(updateProgress, 1000);
 </script>
+
+<style lang="scss" module>
+.progress {
+  :global(.ant-statistic-content) {
+    font-family: monospace;
+  }
+
+  :global(.ant-statistic-content-suffix) {
+    font-size: 75%;
+  }
+}
+</style>
